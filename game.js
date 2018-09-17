e1923fe6532a33808cd8015eca78dd23a09e5173
Viczz <soyviczz@gmail.com>
2018-09-17 22:37:49 +0200

    prueba

---
+++ game.js
@@ -0,0 +1,257 @@
+// define variables
+var game;
+var player;
+var platforms;
+var badges;
+var items;
+var cursors;
+var jumpButton;
+var text;
+var lives = 3;
+var powerup = false;
+var winningMessage;
+var won = false;
+var currentScore = 0;
+var winningScore = 100;
+var enemy;
+var enemy1;
+
+// add collectable items to the game
+function addItems() {
+  items = game.add.physicsGroup();
+
+  createItem(530, 150, 'coin');
+  createItem(250, 500, 'coin');
+  createItem(400, 400, 'coin');
+  createItem(580, 50, 'coin');
+  createItem(380, 250, 'coin');
+  createItem(180, 150, 'coin');
+  createItem(680, 250, 'coin');
+  createItem(280, 20, 'coin');
+  createItem(600,500,'star');
+  createItem(320,500,'poison');
+  createItem(550,60,'poison');
+  createItem(310,200,'poison');
+  
+  
+
+}
+
+// add platforms to the game
+function addPlatforms() {
+  platforms = game.add.physicsGroup();
+  platforms.create(0,0,'fondo');
+  platforms.create(450, 200, 'platform');
+  platforms.create(170 , 550, 'platform_2');
+  platforms.create(320 , 450, 'platform');
+  platforms.create(500, 100, 'platform_2');
+  platforms.create(300 , 300, 'platform');
+  platforms.create(100 , 200, 'platform_2');
+  platforms.create(600, 300, 'platform_2');
+  platforms.create(200, 70, 'platform');
+  
+
+
+  platforms.setAll('body.immovable', true);
+}
+
+// create a single animated item and add to screen
+function createItem(left, top, image) {
+  var item = items.create(left, top, image);
+  item.animations.add('spin');
+  item.animations.play('spin', 10, true);
+}
+
+
+
+// create the winning badge and add to screen
+function createBadge() {
+  badges = game.add.physicsGroup();
+  var badge = badges.create(750, 400, 'badge');
+  badge.animations.add('spin');
+  badge.animations.play('spin', 10, true);
+}
+
+// when the player collects an item on the screen
+function itemHandler(player, item) {
+  
+
+  switch(item.key){
+    case 'star':  powerup = true;
+                  currentScore = currentScore + 20;
+                  item.kill();
+                  break;
+    case 'poison': lives = lives-1;
+    item.kill();
+                   break;
+    case 'fondo': break;
+
+
+    default: currentScore = currentScore + 10;
+    item.kill();
+              if (currentScore === winningScore) {
+                  createBadge();
+                }
+    }
+
+
+
+  
+  
+  
+  
+}
+
+// when the player collects the badge at the end of the game
+function badgeHandler(player, badge) {
+  badge.kill();
+  won = true;
+}
+
+function collisionHandler(player,enemy){
+  enemy.kill();
+  lives = lives-1;
+}
+
+// setup game when the web page loads
+window.onload = function () {
+  game = new Phaser.Game(800 , 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+  
+  // before the game begins
+  function preload() {
+    game.stage.backgroundColor = '#5db1ad';
+    
+    //Load images
+    game.load.image('platform', 'platform_1.png');
+    game.load.image('platform_2', 'platform_2.png');
+    game.load.image('fondo','fondo.png');
+
+    //Load spritesheets
+    game.load.spritesheet('player', 'chalkers.png', 48, 62);
+    game.load.spritesheet('coin', 'coin.png', 36, 44);
+    game.load.spritesheet('badge', 'badge.png', 42, 54);
+    game.load.spritesheet('star','star.png',32,32)
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('mikethefrog','mikethefrog.png',32,32);
+    game.load.spritesheet('enemigos','enemigosxd.png',250,32);
+
+
+
+  }
+
+  // initial game set up
+  function create() {
+
+    addPlatforms();
+    addItems();
+    
+
+    player = game.add.sprite(50, 600, 'player');
+    player.animations.add('walk');
+    player.anchor.setTo(0.5, 1);
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 1000;
+
+    enemy = game.add.sprite(800,600,'mikethefrog');
+    enemy.animations.add('walk');
+    enemy.anchor.setTo(0.5, 1);
+    game.physics.arcade.enable(enemy);
+    enemy.body.collideWorldBounds = true;
+    enemy.body.gravity.y = 1000;
+    enemy.animations.play('walk', 10, true);
+
+    enemy1 = game.add.sprite(220,70,'mikethefrog');
+    enemy1.animations.add('walk');
+    enemy1.anchor.setTo(0.5, 1);
+    game.physics.arcade.enable(enemy1);
+    enemy1.body.collideWorldBounds = true;
+    
+    enemy1.animations.play('walk', 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Arial", fill: "white" });
+    winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white" });
+    winningMessage.anchor.setTo(0.5, 1);
+  }
+
+  // while the game is running
+  function update() {
+
+    if(lives === 0){
+      location.reload();
+    }
+
+    
+
+    text.text = "SCORE: " + currentScore +"\n"+ "LIVES: " + lives;
+
+    game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(player, enemy,collisionHandler);
+    game.physics.arcade.collide(player, enemy1,collisionHandler);
+    game.physics.arcade.overlap(player, items, itemHandler);
+    game.physics.arcade.overlap(player, badges, badgeHandler);
+    player.body.velocity.x = 0;
+
+
+    if(enemy.world.x <= 370){
+      enemy.body.velocity.x = 100;
+      enemy.scale.x = 1;
+
+    } else if(enemy.world.x >= 770){
+      enemy.body.velocity.x = -100;
+      enemy.scale.x = -1;
+    }
+
+    if(enemy1.world.x <= 220){
+      enemy1.body.velocity.x = 100;
+      enemy1.scale.x = 1;
+
+    } else if(enemy1.world.x >= 375){
+      enemy1.body.velocity.x = -100;
+      enemy1.scale.x = -1;
+    }
+
+    if(player.world.x == enemy.world.x ){
+      lives = lives-1;
+    }
+
+     
+
+    // is the left cursor key presssed?
+    if (cursors.left.isDown) {
+      player.animations.play('walk', 10, true);
+      player.body.velocity.x = -300;
+      player.scale.x = - 1;
+    }
+    // is the right cursor key pressed?
+    else if (cursors.right.isDown) {
+      player.animations.play('walk', 10, true);
+      player.body.velocity.x = 300;
+      player.scale.x = 1;
+    }
+    // player doesn't move
+    else {
+      player.animations.stop();
+    }
+    
+    if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
+      if(powerup===true){
+      player.body.velocity.y = -650;
+      } else{
+      player.body.velocity.y = -500;
+
+      }
+    } 
+    // when the player winw the game
+    if (won) {
+      winningMessage.text = "YOU WIN!!!";
+    }
+  }
+
+  function render() {
+
+  }
+
+};
